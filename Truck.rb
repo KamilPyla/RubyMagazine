fa5910a5c21add34e25da39f91ea49af32084258
KamilPyla <kamilpyla@gmail.com>
2021-07-09 16:57:22 +0200

    Refactoring code

--- Truck.rb
+++ Truck.rb
@@ -1,22 +1,22 @@
-require './Vehicle.rb'
+require './Vehicle'
 
 class Truck < Vehicle
-	def initialize (number_of_semitrailer)
-		@semitailer = number_of_semitrailer
-		@semitailer = 2 if number_of_semitrailer >= 2
-		@current_weight = 0
-		@list_of_parcels = []
-	end
+  def initialize (number_of_semitrailer)
+    @semitailer = number_of_semitrailer
+    @semitailer = 2 if number_of_semitrailer >= 2
+    @current_weight = 0
+    @list_of_parcels = []
+  end
 
-	def total_capacity
-		1200 * @semitailer
-	end
+  def total_capacity
+    1200 * @semitailer
+  end
 
-	def start_up_cost
-		1000 + @semitailer * 200
-	end
+  def start_up_cost
+    1000 + @semitailer * 200
+  end
 
-	def cost_per_100km
-		25.3 * 5.6
-	end
+  def cost_per_100km
+    25.3 * 5.6
+  end
 end
--- Van.rb
+++ Van.rb
@@ -1,21 +1,21 @@
 require './Vehicle.rb'
 
 class Van < Vehicle
-	def initialize
-		@cost_per_100km = rand(5.0..8.3) * 5.6 
-		@current_weight = 0
-		@list_of_parcels = []
-	end
+  def initialize
+    @cost_per_100km = rand(5.0..8.3) * 5.6 
+    @current_weight = 0
+    @list_of_parcels = []
+  end
 
-	def total_capacity
-		3000
-	end
+  def total_capacity
+    3000
+  end
 
-	def start_up_cost
-		200
-	end
+  def start_up_cost
+    200
+  end
 
-	def cost_per_100km
-		@cost_per_100km
-	end
+  def cost_per_100km
+    @cost_per_100km
+  end
 end
