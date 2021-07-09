2a71fa0564c1ad96e558e091dc0967a32066866a
KamilPyla <kamilpyla@gmail.com>
2021-07-09 16:56:47 +0200

    Refactoring code

--- Parcel.rb
+++ Parcel.rb
@@ -1,13 +1,14 @@
-class Parcel #Box / package
-	attr_reader :value, :destination, :weight, :id
+# Box / package
+class Parcel
+  attr_reader :value, :destination, :weight, :id
 
-	def initialize(weight, value, destination)
-		@weight, @value = weight, value
-		@destination, @id = destination, @@id
-		@@id += 1
-	end
+  def initialize(weight, value, destination)
+    @weight, @value = weight, value
+    @destination, @id = destination, @@id
+    @@id += 1
+  end
 
-	private
-	@@id = 0
+  private
+  
+  @@id = 0
 end
-
--- Vehicle.rb
+++ Vehicle.rb
@@ -2,33 +2,31 @@
 # plik nazywa się tak jak klasa
 # klasy 100 - 200 linii
 # rubocop
-
-require_relative 'Parcel' # nie potrzebne 
-
 class Vehicle
-	attr_accessor :current_weight, :list_of_parcels
-	def total_capacity
-		raise 'Not implement method!'
-	end
-	
-	def start_up_cost
-		raise 'Not implement method!'
-	end
+  attr_accessor :current_weight, :list_of_parcels
 
-	def cost_per_100km
-		raise 'Not implement method!'
-	end 
+  def total_capacity
+    raise 'Not implement method!'
+  end
 
-	def add_parcel(parcel)
-		if @current_weight <= total_capacity - parcel.weight
-			self.current_weight += parcel.weight
-			self.list_of_parcels << parcel
-		else
-			puts 'Vehicle is full!'
-		end
-	end
+  def start_up_cost
+    raise 'Not implement method!'
+  end
 
-	def cost (distance)
-		start_up_cost + distance/100.0 * cost_per_100km
-	end
+  def cost_per_100km
+    raise 'Not implement method!'
+  end
+
+  def add_parcel(parcel)
+    if @current_weight <= total_capacity - parcel.weight
+      self.current_weight += parcel.weight
+      self.list_of_parcels << parcel
+    else
+      puts 'Vehicle is full!'
+    end
+  end
+
+  def cost(distance)
+    start_up_cost + distance / 100.0 * cost_per_100km
+  end
 end
