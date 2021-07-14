970884b9816014bb2c247f5d68d5c4a965f9a7fa
KamilPyla <kamilpyla@gmail.com>
2021-07-14 12:26:00 +0200

    Add main file

--- main.rb
+++ main.rb
@@ -1,57 +1,13 @@
-require './Train.rb'
-require './Truck.rb'
-require './Van.rb'
-require './Parcel.rb'
-require './Magazine.rb'
-require './Deliver.rb'
+require './train'
+require './truck'
+require './van'
+require './parcel'
+require './magazine'
+require './deliver'
 
-def initialize_trip
-	init = rand(0..15)
-	magazines = []
-	16.times do |localization|
-		magazines << Magazine.new(localization)
-	end
-	10.times{ magazines[init].add_parcel Parcel.new(rand(1..10),rand(20..500), rand(0..15))}
-	return magazines
-end
 
-truck1 = Truck.new(2)
-truck2 = Truck.new(2)
-truck3 = Truck.new(2)
-
-van1 = Van.new
-van2 = Van.new
-van3 = Van.new
-van4 = Van.new
-van5 = Van.new
-
-magazines = initialize_trip
-index = 0
-magazines.each do |m|
-	index = magazines.index(m) if m.list_of_parcels.empty? 
-	p m.list_of_parcels
-end
-p index
-magazine = magazines[index]
-
-p magazine.list_of_parcels
-
-min = Float::INFINITY
-index = 0
-(0..15).each do |i|
-	dist = magazine.shortest_distance_wg(i)
-	p dist
-	p magazine.find_path
-	(0..15).each do |j|
-		path = magazine.find_shortest_path(i,j)
-		p path
-	end
-	if dist.sum < min
-		min = dist.sum
-		index = i
-	end
-end
-p min
-p index
-
-		
+del = Deliver.new(16, rand(0..15))
+del.add_parcels(10_000)
+p del.calculate_weight
+del.calculate_profit
+del.start_trip!
