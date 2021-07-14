78e08f02ec608376304ce528ebed9f6368263aa5
KamilPyla <kamilpyla@gmail.com>
2021-07-14 12:25:28 +0200

    Refactor

--- tsp.rb
+++ tsp.rb
@@ -1,42 +1,41 @@
-def tsp(neighbours, values, v0)
-  @visited = Array.new(values.size, false)
-  @path = Array.new(values.size)
-  @path_temp = Array.new(values.size)
+def tsp(neigh, val, v0)
+  @visited = Array.new(val.size, false)
+  @path = Array.new(val.size)
+  @path_temp = Array.new(val.size)
   @dist = Float::INFINITY
   @shptr = 0
   @dist_temp = 0
-  @n = neighbours.size
+  @n = neigh.size
   @v0 = v0
   
-  def tsp_rec(neighbours, values, v)
+  def tsp_rec(neigh, val, v)
     @path_temp[@shptr] = v
     @shptr += 1
     if @shptr < @n
       @visited[v] = true
 
       (0...@n).each do |u|
-        next if !neighbours[v][u] || @visited[u]
+        next if !neigh[v][u] || @visited[u]
 
-        @dist_temp += values[v][u]
-        tsp_rec(neighbours, values, u)
-        @dist_temp -= values[v][u]
+        @dist_temp += val[v][u]
+        tsp_rec(neigh, val, u)
+        @dist_temp -= val[v][u]
       end
 
       @visited[v] = false
 
-    elsif neighbours[@v0][v]
-      @dist_temp += values[v][@v0]
+    elsif neigh[@v0][v]
+      @dist_temp += val[v][@v0]
 
       if @dist_temp < @dist
         @dist = @dist_temp
         (0...@shptr).each { |i| @path[i] = @path_temp[i] }
       end
-      @dist_temp -= values[v][@v0]
+      @dist_temp -= val[v][@v0]
     end
 
     @shptr -= 1
     @path
   end
-  tsp_rec(neighbours, values, @v0)
-  @path
+  tsp_rec(neigh, val, @v0)
 end
